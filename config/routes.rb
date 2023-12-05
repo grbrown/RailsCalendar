c17084d50909cc5edb3fabfe6c891535e78f0f0c
Graham Brown <graham.brown@flexcar.com>
2023-12-05 11:21:54 -0500

    fix bug

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
+  get 'logout', to: 'sessions#destroy'
   # Defines the root path route ("/")
   # root "posts#index"
 end
