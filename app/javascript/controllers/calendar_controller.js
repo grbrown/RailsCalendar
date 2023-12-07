97b57ff8904529fc0344497d71a16ee7f7206222
Graham Brown <graham.brown@flexcar.com>
2023-12-07 16:15:50 -0500

    clean up controller

--- app/javascript/controllers/calendar_controller.js
+++ app/javascript/controllers/calendar_controller.js
@@ -6,8 +6,7 @@ import interactionPlugin from "@fullcalendar/interaction";
 export default class extends Controller {
   connect() {
     const csrf = document.head.querySelector("meta[name=csrf-token]")?.content;
-    console.log(csrf);
-    console.log("hello from calendar");
+    console.log("rendering calendar");
     var calendarEl = document.getElementById("calendar");
     var calendar = new Calendar(calendarEl, {
       plugins: [dayGridPlugin, interactionPlugin],
@@ -18,27 +17,20 @@ export default class extends Controller {
         right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek",
       },
       dateClick: function (info) {
-        // alert("Clicked on: " + info.dateStr);
-        // alert("Coordinates: " + info.jsEvent.pageX + "," + info.jsEvent.pageY);
-        // alert("Current view: " + info.view.type);
-        // change the day's background color just for fun
-        console.log(info);
-        info.dateStr;
         const myEvent = {
-          title: "mymeeting", // a property!
-          start: info.dateStr, // a property!
-          end: info.dateStr, // a property! ** see important note below about 'end' **
+          title: "mymeeting",
+          start: info.dateStr,
+          end: info.dateStr,
         };
         const req = new Request("/api/v1/events", {
           method: "POST",
           body: JSON.stringify(myEvent),
           headers: { "Content-Type": "application/json", "X-CSRF-Token": csrf },
         });
-        //req.method = "POST";
-        fetch(req).then((response) => console.log(response.json()));
+        fetch(req).then((response) => {
+          console.log(response.json());
+        });
         calendar.addEvent(myEvent);
-        info.dayEl.style.backgroundColor = "red";
-        console.log(calendar.getEvents());
       },
     });
 
@@ -47,9 +39,8 @@ export default class extends Controller {
     fetch("/api/v1/events")
       .then((response) => response.json())
       .then((events) => {
-        console.log(events);
+        console.log("events", events);
         events.forEach((event) => {
-          console.log(JSON.stringify(event));
           calendar.addEvent({
             title: event.title,
             start: event.start,
@@ -57,7 +48,6 @@ export default class extends Controller {
           });
         });
       });
-
-    fetch("/posts.json").then((response) => console.log(response.json()));
+    console.log("calendar render complete");
   }
 }
